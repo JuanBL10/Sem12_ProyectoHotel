946bf19e51f6d9d12f2d78fde817028c209760d2
Juan <juan.barrantesloria@gmail.com>
2026-05-28 11:10:21 -0600

    Cambios peques solo para devolverme, en editarHotel btn

--- Controllers/hotelController.mjs
+++ Controllers/hotelController.mjs
@@ -1,6 +1,7 @@
 import Hotel from '../Models/hotel.mjs';
 
 const URL = "https://paginas-web-cr.com//Api/hotelApi/hotel/hotel.php";
+const arrayHoteles = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarHoteles();
@@ -27,15 +28,31 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('botonCerrar').addEventListener('click', (evento) => {
         document.getElementById('formAgregarHotel').reset();
     });
+
+    document.querySelector('#modalEditarHotel .btn-close').addEventListener('click', evento => {
+        document.getElementById('formEditarHotel').reset();
+    });
+    
+    document.querySelector('#modalEditarHotel .btn-secondary').addEventListener('click', evento => {
+        document.getElementById('formEditarHotel').reset();
+    });
+
+    document.getElementById('formEditarHotel').addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        enviarDatosEditar();
+    });
 });
 
 async function consultarHoteles(){
+    //Copia de hoteles en array para el editar
+    arrayHoteles = [];
     try {
         const response = await fetch(URL, {
             method: "GET"
         });
         const data = await response.json();
         console.log(data.data);
+        arrayHoteles.push(data.data);
         dibujarTabla(data.data);
     }
     catch (error) {
@@ -85,10 +102,15 @@ function dibujarTabla(dataHoteles) {
             <td>
                 <div class="container-fluid">
                     <div class="row pb-1">
-                        <button class="btn btn-warning btn-sm">Editar</button>
+                        <button type="button" class="btn btn-warning btn-sm" 
+                        data-bs-toggle="modal" data-bs-target="#modalEditarHotel"
+                        onclick="abrirModalEditarHotel(${hotel.id}, '${hotel.nombre}', 
+                        '${hotel.descripcion}', '${hotel.telefono}', 
+                        '${hotel.correo}', '${hotel.sitio_web}', 
+                        '${hotel.usuario}')">Editar</button>
                     </div>
                     <div class="row pt-1">
-                        <button class="btn btn-danger btn-sm">Eliminar</button>
+                        <button class="btn btn-danger btn-sm" onclick="eliminarHotel(${hotel.id})">Eliminar</button>
                     </div>
                 </div>
             </td>
@@ -124,5 +146,45 @@ async function enviarDatosAgregar(){
     }
     catch (error) {
         console.error("Error al agregar el hotel:", error);
+    }
+}
+
+function abrirModalEditarHotel(id, nombre, descripcion, telefono, correo, sitioWeb, usuario) {
+    //Esto es para que se llene el modal con los datos existentes del hotel
+    document.getElementById('idHotelEditar').value = id;
+    document.getElementById('nombreHotelEditar').value = nombre;
+    document.getElementById('descripcionHotelEditar').value = descripcion;
+    document.getElementById('telefonoHotelEditar').value = telefono;
+    document.getElementById('correoHotelEditar').value = correo;
+    document.getElementById('sitioWebHotelEditar').value = sitioWeb;
+    document.getElementById('usuarioEditar').value = usuario;
+}
+
+async function enviarDatosEditar(){
+    const hotelEditar = new Hotel(
+        document.getElementById('idHotelEditar').value,
+        document.getElementById('nombreHotelEditar').value,
+        document.getElementById('descripcionHotelEditar').value,
+        document.getElementById('telefonoHotelEditar').value,
+        document.getElementById('correoHotelEditar').value,
+        document.getElementById('sitioWebHotelEditar').value,
+        document.getElementById('usuarioEditar').value
+    );
+
+    try{
+        const response = await fetch(URL, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"},
+            body: JSON.stringify(hotelEditar)
+        });
+        const data = await response.json();
+        console.log(data);
+        alert("Hotel editado exitosamente");
+        document.getElementById('modalEditarHotel').querySelector('.btn-close').click();
+        consultarHoteles();
+    }
+    catch(error){
+        console.error("Error al editar el hotel:", error);
     }
 }
